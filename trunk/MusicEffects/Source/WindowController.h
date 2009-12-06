d1b2c0138314fdfa6a7915a0d8920c4d934ed489
doughyde <doughyde@cdbb82e2-d0dd-11de-8184-f791cb96e2ed>
2009-12-06 00:09:19 +0000

    MusicEffects

---
+++ trunk/MusicEffects/Source/WindowController.h
@@ -0,0 +1,79 @@
+//
+//  AudioUnitWrapper.h
+//  MusicEffect
+//
+//  Created by Doug Hyde on 10/3/09.
+//  Copyright 2009 Washington University in St. Louis. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+#include <AudioToolbox/AudioToolbox.h>
+#include <AudioUnit/AudioUnit.h>
+
+#import "AudioFileReceiver_Protocol.h"
+
+@class AudioFileListView;
+class CAComponent;
+
+#define MAX_UNITS 12
+
+@interface HostingWindowController : NSWindowController <AudioFileReceiver, NSBrowserDelegate> {
+	
+    IBOutlet NSButton *				uiAudioFileButton;
+    IBOutlet NSPopUpButton *		uiAUPopUpButton;
+    IBOutlet NSBox *				uiAUViewContainer;
+	
+	IBOutlet NSBrowser *			audioUnitBrowser;
+    
+	IBOutlet NSButton *				uiPlayStop;
+	
+
+    IBOutlet NSButton *				uiPlayStopButton;
+    IBOutlet AudioFileListView *	uiAudioFileTableView;
+    IBOutlet NSTextField *			uiAudioFileNowPlayingName;
+    
+    NSScrollView *					mScrollView;
+    
+    NSMutableArray *				mAudioFileList;
+
+	AudioFileID						mAFID;
+	AUGraph							mGraph;
+	AUNode							mFileNode, mixerNode, mOutputNode;
+	AudioUnit						mFileUnit, mixerUnit, mOutputUnit;
+	
+	CAComponent *					allAudioUnits;
+	AudioUnit						activeUnits[MAX_UNITS];
+	AUNode							activeNodes[MAX_UNITS];
+	NSString *						activeNames[MAX_UNITS];
+	int								numActiveUnits;
+}
+
+
+- (void)stopGraph;
+
+#pragma mark IB Actions
+- (IBAction) addAudioUnit:(id)sender;
+- (IBAction)iaPlayStopButtonPressed:(id)sender;
+
+- (IBAction) stopMusic: (id)sender;
+
+
+
+
++ (BOOL)plugInClassIsValid:(Class) pluginClass;
+- (void)cleanup;
+- (void)createGraph;
+- (void)startGraph;
+- (void)stopGraph;
+//- (void)connectGraph;
+- (void)destroyGraph;
+- (void)showAudioUnit:(AudioUnit)inAU;
+- (void)prepareFileAudioUnit;
+- (void)synchronizePlayStopButton;
+- (void)buildAudioUnitList;
+- (void)addLinkToFiles:(NSArray *)inFiles;
+- (void)loadAudioFile:(NSString *)inAudioFileName;
+- (void)handleDoubleClick: (id)sender;
+
+@end
